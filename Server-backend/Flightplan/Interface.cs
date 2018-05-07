257f2f529ee73e9969e36b4df3e3f4edd3a01189
Dstoft <Dstoft@hotmail.com>
2018-05-07 23:33:42 +0200

    Suggested folder structure setup.

---
+++ Server-backend/Flightplan/Interface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server_backend.Flightplan
+{
+    interface IFlightplan
+    {
+        List<Command> GetPossibleCommands();
+        List<Command> GetCommands(int flightplanId);
+        void AddCommand(Command cmd);
+    }
+}
